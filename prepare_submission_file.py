9fae8e4893f905a18d205c9883348a6d11df7452
Rohan Rao <rohanrao88@gmail.com>
2016-12-23 22:34:42 +0530

    removed extra space

--- prepare_submission_file.py
+++ prepare_submission_file.py
@@ -9,9 +9,9 @@ import pandas as pd
 
 # preparing submission file
 target_cols = ['ind_cco_fin_ult1','ind_cder_fin_ult1','ind_cno_fin_ult1','ind_ctju_fin_ult1',
-			   'ind_ctma_fin_ult1','ind_ctop_fin_ult1','ind_ctpp_fin_ult1','ind_dela_fin_ult1',
-			   'ind_ecue_fin_ult1','ind_fond_fin_ult1','ind_hip_fin_ult1','ind_plan_fin_ult1',
-			   'ind_pres_fin_ult1','ind_reca_fin_ult1','ind_tjcr_fin_ult1','ind_valo_fin_ult1',
+	       'ind_ctma_fin_ult1','ind_ctop_fin_ult1','ind_ctpp_fin_ult1','ind_dela_fin_ult1',
+	       'ind_ecue_fin_ult1','ind_fond_fin_ult1','ind_hip_fin_ult1','ind_plan_fin_ult1',
+	       'ind_pres_fin_ult1','ind_reca_fin_ult1','ind_tjcr_fin_ult1','ind_valo_fin_ult1',
                'ind_nomina_ult1','ind_nom_pens_ult1','ind_recibo_ult1']
 
 last_instance_df = pd.read_csv(path_train, usecols=['ncodpers'] + target_cols)
